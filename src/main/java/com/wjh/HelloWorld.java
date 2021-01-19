cc4ab4489b384bf3d4c8615d8c4909f5a3f1462e
709555630@qq.com <lengxue19910621>
2021-01-19 17:26:52 +0800

    master第二次提交

--- src/main/java/com/wjh/HelloWorld.java
+++ src/main/java/com/wjh/HelloWorld.java
@@ -2,6 +2,7 @@ package com.wjh;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(2222);
+        int i=10;
+        System.out.println(1111);
     }
 }
